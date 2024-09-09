172db436c70e72b1770f4d1c784746d4a9432c98
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-09 05:04:17 +0000

    cleanup

--- install.sh
+++ install.sh
@@ -10,11 +10,11 @@ echo "Installing terminal and desktop tools.."
 # Install terminal tools
 source ~/.local/share/omakub/install/terminal.sh
 
-if $RUNNING_GNOME; then
-  # Install desktop tools and tweaks
-  source ~/.local/share/omakub/install/desktop.sh
+# Install desktop tools and tweaks
+source ~/.local/share/omakub/install/desktop.sh
 
-  # Revert to normal idle and lock settings
-  gsettings set org.gnome.desktop.screensaver lock-enabled true
-  gsettings set org.gnome.desktop.session idle-delay 300
-fi
+# Revert to normal idle and lock settings
+gsettings set org.gnome.desktop.screensaver lock-enabled true
+gsettings set org.gnome.desktop.session idle-delay 300
+
+sudo reboot
