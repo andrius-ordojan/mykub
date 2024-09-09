7e65349e06cc89027de635be15c206eb36927a1b
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-09 04:43:47 +0000

    installed server and pgcli

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -1,3 +1,3 @@
 sudo apt install -y \
   build-essential pkg-config autoconf bison clang rustc \
-  sqlite3 libsqlite3-0 libpq-dev postgresql-client postgresql-client-common
+  sqlite3 libsqlite3-0 
---
+++ install/terminal/postgresql.sh
@@ -0,0 +1,4 @@
+sudo apt install postgresql postgresql-contrib libpq-dev postgresql-client postgresql-client-common pgcli
+
+sudo systemctl start postgresql.service
+sudo systemctl enable postgresql.service
