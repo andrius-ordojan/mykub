3b2c8456807d88a26b103e5f1561a1a94211a897
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-09 22:37:28 +0000

    adding extra user creation

--- install/terminal/postgresql.sh
+++ install/terminal/postgresql.sh
@@ -1,1 +1,5 @@
 sudo apt install -y postgresql postgresql-contrib libpq-dev postgresql-client postgresql-client-common pgcli
+
+# TODO: enable password auth (md5) instead of peer
+sudo -u postgres createuser --superuser andrius
+sudo -u postgres psql -c "ALTER USER andrius PASSWORD 'andrius';"
