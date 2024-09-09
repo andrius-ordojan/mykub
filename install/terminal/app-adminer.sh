8ba948b7fae4c21e205cb06d891cdb5af7aa9bd3
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-09 22:37:11 +0000

    todo

--- install/terminal/app-adminer.sh
+++ install/terminal/app-adminer.sh
@@ -1,1 +1,2 @@
-sudo docker run -d -it --restart unless-stopped -p "127.0.0.1:8080:80" --name=adminer -e ADMINER_SERVER=postgres16 -e ADMINER_USERNAME=postgres -e ADMINER_PASSWORD=postgres dockette/adminer:dg
+# TODO: configure either local nginx or caddy and deploy this or make docker work
+#sudo docker run -d -it --restart unless-stopped  -p "127.0.0.1:8080:80" --name=adminer -e ADMINER_SERVER=postgres16 -e ADMINER_USERNAME=postgres -e ADMINER_PASSWORD=postgres dockette/adminer:dg
