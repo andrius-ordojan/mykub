7373f6f45a63baf454d3a142c10c4f7f50da5728
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-09 04:48:31 +0000

    user config rebase true

--- install/terminal/set-git.sh
+++ install/terminal/set-git.sh
@@ -1,6 +1,11 @@
-# Set common git aliases
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
 git config --global pull.rebase true
+
+git config --global user.email "andrius.or@gmail.com"
+git config --global user.name "Andrius Ordojan"
+
+git config --global pull.rebase true
+
