c3802036806400a4478e7269f40a457ce7721352
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-09 22:43:26 +0000

    notes

--- install/terminal/app-adminer.sh
+++ install/terminal/app-adminer.sh
@@ -1,2 +1,3 @@
 # TODO: configure either local nginx or caddy and deploy this or make docker work
+# try using psql from container to see if i can reach it using username and passowrd auth
 #sudo docker run -d -it --restart unless-stopped  -p "127.0.0.1:8080:80" --name=adminer -e ADMINER_SERVER=postgres16 -e ADMINER_USERNAME=postgres -e ADMINER_PASSWORD=postgres dockette/adminer:dg
