eb090efaf63c4434c9d44a485b16e2dd1211ffa4
Andrius Ordojan <aor@templafy.com>
2024-09-09 15:53:12 +0200

    moving things

--- install/terminal/libraries.sh
+++ install/terminal/libraries.sh
@@ -1,1 +1,1 @@
-sudo apt install -y build-essential pkg-config autoconf bison clang rustc make gcc sqlite3 libsqlite3-0
+sudo apt install -y build-essential pkg-config autoconf bison clang rustc make gcc
