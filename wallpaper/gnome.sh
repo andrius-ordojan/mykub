05283ba78a0366559b1d8039fe8a16016700ef66
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-09 16:10:01 +0000

    adding wallpaper setup

---
+++ wallpaper/gnome.sh
@@ -0,0 +1,5 @@
+# TODO: what does this do?
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+gsettings set org.gnome.desktop.background picture-uri-dark ./background.jpg
+gsettings set org.gnome.desktop.background picture-options 'zoom'
