14ca6d54bb3b0e3e11e6f405ae293d65caa15dde
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-10 12:03:21 +0200

    Update apps-caddy.sh

--- install/terminal/apps-caddy.sh
+++ install/terminal/apps-caddy.sh
@@ -3,7 +3,13 @@ sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https cu
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update
+
+# TODO: might not need php
 sudo apt install -y caddy php php-fpm
+
+# TODO: make variable for user
+sudo usermod -aG andrius caddy
+
 
 mkdir -p ~/.local/share/mykub/apps/adminer/
 wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php -O ~/.local/share/mykub/apps/adminer/adminer-4.8.1.php
