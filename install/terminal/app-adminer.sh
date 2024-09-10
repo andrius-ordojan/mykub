022c946b4709f37831dfab838c430a2986beda46
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-10 04:56:48 +0000

    installing caddy

--- install/terminal/app-adminer.sh
+++ install/terminal/app-adminer.sh
@@ -1,3 +1,7 @@
-# TODO: configure either local nginx or caddy and deploy this or make docker work
-# try using psql from container to see if i can reach it using username and passowrd auth
-#sudo docker run -d -it --restart unless-stopped  -p "127.0.0.1:8080:80" --name=adminer -e ADMINER_SERVER=postgres16 -e ADMINER_USERNAME=postgres -e ADMINER_PASSWORD=postgres dockette/adminer:dg
+# installing caddy and php
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install -y caddy php php-fpm
+
