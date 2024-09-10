fc2a63020674a7991d6e60a20c75b0d961051460
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-10 12:37:16 +0200

    todo

--- install/terminal/postgresql.sh
+++ install/terminal/postgresql.sh
@@ -1,5 +1,7 @@
 sudo apt install -y postgresql postgresql-contrib libpq-dev postgresql-client postgresql-client-common pgcli
 
-# TODO: enable password auth (md5) instead of peer
+# TODO: enable password auth (md5) instead of peer for adminer
+# TODO: create database user
+# TODO: use variable for user name
 sudo -u postgres createuser --superuser andrius
 sudo -u postgres psql -c "ALTER USER andrius PASSWORD 'andrius';"
