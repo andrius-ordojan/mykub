ebdeac2f7e672d4bafccfd2c12132c28b9d001e8
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-10 05:14:23 +0000

    setting up adminer

--- install/terminal/apps-caddy.sh
+++ install/terminal/apps-caddy.sh
@@ -5,3 +5,18 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo 
 sudo apt update
 sudo apt install -y caddy php php-fpm
 
+mkdir -p ~/.local/share/mykub/apps/adminer/
+wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php -O ~/.local/share/mykub/apps/adminer/adminer.php
+
+
+cat <<EOL > /etc/caddy/Caddyfile
+localhost:80 {
+    handle_path /adminer/* {
+        root * ~/.local/share/mykub/apps/adminer/
+        php_fastcgi unix//run/php/php-fpm.sock
+        file_server
+    }
+}
+EOL
+
+sudo systemctl restart caddy
