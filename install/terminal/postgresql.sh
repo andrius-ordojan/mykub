da6e90ac0e9943c0ad85374e532cf23999da0e16
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-10 13:12:22 +0200

    Update postgresql.sh

--- install/terminal/postgresql.sh
+++ install/terminal/postgresql.sh
@@ -1,7 +1,17 @@
-sudo apt install -y postgresql postgresql-contrib libpq-dev postgresql-client postgresql-client-common pgcli
+sudo apt install -y postgresql-16 postgresql-contrib libpq-dev postgresql-client postgresql-client-common pgcli
 
 # TODO: enable password auth (md5) instead of peer for adminer
 # TODO: create database user
 # TODO: use variable for user name
 sudo -u postgres createuser --superuser andrius
 sudo -u postgres psql -c "ALTER USER andrius PASSWORD 'andrius';"
+
+
+/etc/postgresql/16/main/pg_hba.conf
+host    all             all             127.0.0.1/32            md5
+change from peer
+
+
+/etc/postgresql/16/main/postgresql.conf
+listen_addresses = 'localhost'
+uncomment
