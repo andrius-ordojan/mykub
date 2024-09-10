f3926b8ef5d2397aaee26206e3cbddb7f1b75b16
Andrius Ordojan <andrius.or@gmail.com>
2024-09-10 21:37:17 +0200

    configured postgres

--- install/terminal/postgresql.sh
+++ install/terminal/postgresql.sh
@@ -1,18 +1,10 @@
 sudo apt install -y postgresql-16 postgresql-contrib libpq-dev postgresql-client postgresql-client-common pgcli
 
-# TODO: enable password auth (md5) instead of peer for adminer
-# TODO: create database user
-# TODO: use variable for user name
-sudo -u postgres createuser --superuser andrius
-sudo -u postgres psql -c "ALTER USER andrius PASSWORD 'andrius';"
+sudo -u postgres createuser --superuser $USER
+sudo -u postgres psql -c "ALTER USER $USER PASSWORD '$USER';"
 
+# TODO: double check the line number
+sed -i '125s/peer/md5/' /etc/postgresql/16/main/pg_hba.conf
 
-/etc/postgresql/16/main/pg_hba.conf
-host    all             all             127.0.0.1/32            md5
-change from peer
-
-
-/etc/postgresql/16/main/postgresql.conf
-listen_addresses = 'localhost'
-change port  to 5432
-uncomment
+sudo sed -i "s/port = 5433/port = 5432/" /etc/postgresql/16/main/postgresql.conf
+sudo sed -i "/^#listen_addresses = 'localhost'/s/^#//" /etc/postgresql/16/main/postgresql.conf
