8e842041cb5aa4633812289b119879dbaa84abe3
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-11 05:21:43 +0000

    removed line change

--- install/terminal/postgresql.sh
+++ install/terminal/postgresql.sh
@@ -3,8 +3,5 @@ sudo apt install -y postgresql-16 postgresql-contrib libpq-dev postgresql-client
 sudo -u postgres createuser --superuser $USER
 sudo -u postgres psql -c "ALTER USER $USER PASSWORD '$USER';"
 
-# TODO: double check the line number
-sed -i '125s/peer/md5/' /etc/postgresql/16/main/pg_hba.conf
-
 sudo sed -i "s/port = 5433/port = 5432/" /etc/postgresql/16/main/postgresql.conf
 sudo sed -i "/^#listen_addresses = 'localhost'/s/^#//" /etc/postgresql/16/main/postgresql.conf
