1a3d6d53ec59863f3186dd8daaa9b88ea9270100
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-11 19:57:41 +0000

    wezterm

--- install/desktop/app-wezterm.sh
+++ install/desktop/app-wezterm.sh
@@ -1,9 +1,4 @@
-# TODO: change to wezterm 
-sudo apt install -y alacritty
-mkdir -p ~/.config/alacritty
-cp ~/.local/share/mykub/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
-cp ~/.local/share/mykub/themes/tokyo-night/alacritty.toml ~/.config/alacritty/theme.toml
-cp ~/.local/share/mykub/configs/alacritty/fonts/CaskaydiaMono.toml ~/.config/alacritty/font.toml
-cp ~/.local/share/mykub/configs/alacritty/font-size.toml ~/.config/alacritty/font-size.toml
-
-# TODO: install add config add font
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo apt update
+sudo apt install wezterm
