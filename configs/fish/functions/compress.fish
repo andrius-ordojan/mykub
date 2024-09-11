8d38bde33f60a4f9baf28db4effe69a8297bfdf5
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-11 23:37:26 +0000

    deleted configs

--- configs/fish/functions/compress.fish
+++
@@ -1,4 +0,0 @@
-function compress
-    set -l dir_name $argv[1]
-    tar -czf (string trim -r "/" $dir_name).tar.gz (string trim -r "/" $dir_name)
-end
--- configs/storagesetup.sh
+++
@@ -1,3 +0,0 @@
-docker run -d --restart unless-stopped --network local -p "127.0.0.1:8080:8080" --name=adminer -e ADMINER_DEFAULT_USER="postgres" -e ADMINER_DEFAULT_SERVER=localhost  adminer
-docker run -d --restart unless-stopped --network local -p "127.0.0.1:5432:5432" --name=postgres16 -e POSTGRES_HOST_AUTH_METHOD=trust postgres:16.3-bookworm
-docker run -d -it --restart unless-stopped --network local -p "127.0.0.1:8080:80" --name=adminer -e ADMINER_SERVER=postgres16 -e ADMINER_USERNAME=postgres -e ADMINER_PASSWORD=postgres dockette/adminer:dg
