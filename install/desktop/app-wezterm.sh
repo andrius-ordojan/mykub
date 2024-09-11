cc4e4d6989b9d536d693ae14f59a849042ab2b66
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-11 19:58:23 +0000

    yes

--- install/desktop/app-wezterm.sh
+++ install/desktop/app-wezterm.sh
@@ -1,4 +1,4 @@
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo apt update
-sudo apt install wezterm
+sudo apt -y install wezterm
