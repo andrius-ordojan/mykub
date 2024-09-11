585a8832e87a7cabb8895b97f6b4356d1177056a
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-11 22:45:34 +0000

    set wallpaper

---
+++ install/desktop/set-wallpaper.sh
@@ -0,0 +1,3 @@
+BACKGROUND_DEST="$HOME/.local/share/mykub/wallpaper/green.jpg"
+gsettings set org.gnome.desktop.background picture-uri-dark $BACKGROUND_DEST
+gsettings set org.gnome.desktop.background picture-options 'zoom'
--- wallpaper/set-gnome.sh
+++
@@ -1,5 +0,0 @@
-# TODO: what does this do?
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-
-gsettings set org.gnome.desktop.background picture-uri-dark ./background.jpg
-gsettings set org.gnome.desktop.background picture-options 'zoom'
