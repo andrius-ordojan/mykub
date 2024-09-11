fb4ff7381d52eb9c9422d076677dc46d8dd3f37f
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-11 23:13:25 +0000

    chaning default font

--- install/desktop/set-gnome-settings.sh
+++ install/desktop/set-gnome-settings.sh
@@ -1,5 +1,7 @@
 # Center new windows in the middle of the screen
 gsettings set org.gnome.mutter center-new-windows true
 
+gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMono Nerd Font Mono 12'
+
 # Reveal week numbers in the Gnome calendar
 gsettings set org.gnome.desktop.calendar show-weekdate true
