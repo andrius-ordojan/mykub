1c96af477c5d4af86fea7104d94f01c251bc1d1d
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-15 19:41:32 +0200

    Update set-dock.sh

--- install/desktop/set-dock.sh
+++ install/desktop/set-dock.sh
@@ -1,1 +1,1 @@
-gsettings set org.gnome.shell favorite-apps "['firefox_firefox.desktop', 'org.wezfurlong.wezterm.desktop', 'obsidian_obsidian.desktop', 'org.gnome.Nautilus.desktop']"
+gsettings set org.gnome.shell favorite-apps "['firefox_firefox.desktop', 'org.wezfurlong.wezterm.desktop', 'obsidian_obsidian.desktop', 'io.bassi.Amberol.desktop']"
