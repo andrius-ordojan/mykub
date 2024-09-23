c681d4bda1be9680cb868e1605e26b48f927448e
Andrius Ordojan <Andrius.or@gmail.com>
2024-09-23 14:23:07 +0200

    Update apps-terminal.sh

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,4 +1,4 @@
-sudo apt install -y fzf ripgrep bat eza zoxide plocate btop fd-find tldr
+sudo apt install -y fzf ripgrep bat eza zoxide plocate btop fd-find tldr sd
 
 ln -s $(which fdfind) ~/.local/bin/fd
 ln -s $(which batcat) ~/.local/bin/bat
